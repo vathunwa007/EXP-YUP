2897b5269d8db0b37ab90a082e28ef443431e168
thunwa moh <thunwa_moh@thaivivat.co.th>
2024-11-20 18:41:53 +0700

    init Project

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Yup Validate Login</h1>
+      <div className="card">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
